030487cb6dca76be18ef3b8284b0596cc471967c
21CEUOS101 <120437060+21CEUOS101@users.noreply.github.com>
2023-07-07 03:45:16 +0000

    First

---
+++ Program.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press 1 for Addition");
+            Console.WriteLine("Press 2 for Subtraction");
+            Console.WriteLine("Press 3 for Multiplication");
+            Console.WriteLine("Press 4 for Division");
+            int operation = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter First Operand");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second Operand");
+            int b = int.Parse(Console.ReadLine());
+
+            if(operation == 1)
+            {
+                int ans = a + b;
+                Console.WriteLine("Answer : " + ans);
+            }
+            else if(operation == 2)
+            {
+                int ans = a - b;
+                Console.WriteLine("Answer : " + ans);
+            }
+            else if(operation == 3)
+            {
+                int ans = a * b;
+                Console.WriteLine("Answer : " + ans);
+            }
+            else if (operation == 4)
+            {
+                int ans = a / b;
+                Console.WriteLine("Answer : " + ans);
+            }
+        }
+    }
+}
